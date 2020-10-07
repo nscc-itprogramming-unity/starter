51ec5edf1e3ff8acc599983df0833fce85ff8b22
Alex Young <31460377+AlexTGYoung@users.noreply.github.com>
2020-10-07 14:06:39 -0300

    changed menu colors

--- Assets/Scripts/oxygenControl.cs
+++ Assets/Scripts/oxygenControl.cs
@@ -6,27 +6,27 @@ using UnityEngine.UI;
 public class oxygenControl : MonoBehaviour
 {
     public Slider oxygenBar;
-    public floatValue oxygen;
+    public floatValue inspiration;
     public GameObject youDied;
     // Start is called before the first frame update
     void Start()
     {
-        oxygen.value = 100;
+        inspiration.value = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
         //relates the oxygen bar value to the oxygen value
-        oxygenBar.value = oxygen.value;
+        oxygenBar.value = inspiration.value;
 
         //drains oxygen bar
-        if (oxygen.value > 0)
+        if (inspiration.value > 0)
         {
-            oxygen.value -= 2 * Time.deltaTime;
+            inspiration.value -= 2 * Time.deltaTime;
         }
         //checks if suffocated
-        if(oxygen.value <= 0)
+        if(inspiration.value <= 0)
         {
             Time.timeScale = 0;
             youDied.SetActive(true);
